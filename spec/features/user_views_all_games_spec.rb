d6dae01145036b9ff449818a99e4136d632802d6
Kelsey Hale <kh.khale@gmail.com>
2015-07-11 15:56:23 -0400

    add games table, games index

--- spec/features/user_views_all_games_spec.rb
+++ spec/features/user_views_all_games_spec.rb
@@ -4,7 +4,7 @@ feature "As a user
   I want to view a list of created games
   So that I can decide to join one." do
   scenario 'user visits page and sees a list of all active games' do
-    Game.new(name: "The Killing Game")
+    Game.create!(name: "The Killing Game")
     visit '/'
     click_on "View created games"
     expect(page).to have_content "Appsassin"
