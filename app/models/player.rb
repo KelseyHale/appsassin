4417c31ccc486696a79355822fdf3224981e6417
Kelsey Hale <kh.khale@gmail.com>
2015-07-15 12:46:04 -0400

    add unit test and method to generate list of possible targets for a given player

--- app/models/player.rb
+++ app/models/player.rb
@@ -4,4 +4,15 @@ class Player < ActiveRecord::Base
 
   validates :user, presence: true
   validates :game, presence: true
+
+  def generate_targets(current_player)
+    players = Player.all
+    possible_targets = []
+    players.each do |player|
+      unless current_player.user_id == player.user_id
+        possible_targets << player.user_id
+      end
+    end
+    possible_targets
+  end
 end
---
+++ spec/models/player_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe Player, type: :model do
+  it { should belong_to(:user) }
+  it { should belong_to(:game) }
+
+  describe '.generate_target' do
+    context 'generates possible targets from active players' do
+      it 'returns an array of possible targets and does not include inactive' do
+        user = FactoryGirl.create(:user)
+        user2 = FactoryGirl.create(:user)
+        user3 = FactoryGirl.create(:user)
+        game = FactoryGirl.create(:game)
+        player = Player.create(user: user, game: game)
+        player2 = Player.create(user: user2, game: game)
+        player3 = Player.create(user: user3, game: game)
+        # binding.pry
+        expect(player.generate_targets(player)).to eq [player2.user_id, player3.user_id]
+      end
+    end
+  end
+end
