5cbf1025a87ef975e12b368a42e4bae35dcf0d2a
Kelsey Hale <kh.khale@gmail.com>
2015-07-18 19:26:34 -0400

    update feature test eliminate player

--- spec/features/user_marks_target_as_eliminated_spec.rb
+++ spec/features/user_marks_target_as_eliminated_spec.rb
@@ -2,9 +2,11 @@ require 'rails_helper'
 
 # Acceptance criteria
 #
-# []
-# []
-# []
+# [] User must be logged in
+# [] User must be on the player dashboard
+# [] User hovers over picture and sees option to mark as eliminated
+# [] User recieves a confirmation message to eliminate player
+# [] User clicks confirmation message and player is marked as eliminated on the player's dashboard and in the game show page
 
 feature "As a user
 I want to mark my target as eliminated
@@ -30,7 +32,13 @@ So other players know who is out of the game" do
     fill_in "Password", with: user.password
     click_button "Log in"
 
-    click_link
+    click_link "Target eliminated"
+    expect(page).to have_content "Please confirm that player has been eliminated"
+    click_button "Confirm"
+    expect(page).to have_content ""
+
+    visit game_path(game)
+    expect(page).to have_content ""
 
     expect(page).to have_content "Your current targets:"
     expect(page).to have_content target.user.first_name
