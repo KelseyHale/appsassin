9c30fc1c17c118ca1d7a0aa78bf73d17f55e5bc9
Kelsey Hale <kh.khale@gmail.com>
2015-07-22 14:23:43 -0400

    refactor

--- app/models/player.rb
+++ app/models/player.rb
@@ -8,21 +8,6 @@ class Player < ActiveRecord::Base
   validates :user, presence: true
   validates :game, presence: true
 
-  def generate_targets(current_player)
-    targets = Target.where(game_id: current_player.game_id, active: true)
-    possible_targets = []
-    targets.each do |target|
-      if current_player.user_id != target.user_id
-        possible_targets << target
-      end
-    end
-    possible_targets #= possible_targets - used_targets(current_player)
-  end
-
-  def assign_target_to_player(current_player)
-    generate_targets(current_player).sample
-  end
-
   def target
     self.game.rounds.last.round_assignments.where(player_id: self).last.target
   end
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -4,61 +4,18 @@ RSpec.describe Player, type: :model do
   it { should belong_to(:user) }
   it { should belong_to(:game) }
 
-  describe '.generate_target' do
-    context 'generates possible targets from active players' do
-      it 'returns an array of possible targets and does not include inactive' do
-        user = FactoryGirl.create(:user)
-        user2 = FactoryGirl.create(:user)
-        user3 = FactoryGirl.create(:user)
-        user4 = FactoryGirl.create(:user)
-        FactoryGirl.create(:user)
-        game = FactoryGirl.create(:game)
-        game2 = FactoryGirl.create(:game)
-        player = Player.create(user: user, game: game)
-        Player.create(user: user2, game: game)
-        Player.create(user: user3, game: game)
-        Player.create(user: user4, game: game, active: false)
-        Player.create(user: user3, game: game2)
-        Target.create(user: user, game: game)
-        target2 = Target.create(user: user2, game: game)
-        target3 = Target.create(user: user3, game: game)
-        Target.create(user: user4, game: game, active: false)
-        Target.create(user: user4, game: game2)
-
-        expect(player.generate_targets(player)).to eq [
-          target2,
-          target3
-        ]
-      end
-    end
-    # context --- need to test assign_target_to_player --- test for random??
-  end
   describe '.target' do
     context 'a player as an assigned target' do
       it 'returns the name of the target' do
         user = FactoryGirl.create(:user)
         user2 = FactoryGirl.create(:user)
-
-        # user3 = FactoryGirl.create(:user)
-        # user4 = FactoryGirl.create(:user)
-
         game = FactoryGirl.create(:game)
         player1 = Player.create!(user: user, game: game)
-        player2 = Player.create!(user: user2, game: game)
-
-        # Player.create!(user: user3, game: game)
-        # Player.create!(user: user4, game: game, active: false)
-
-        target = Target.create!(user: user, game: game)
+        Player.create!(user: user2, game: game)
+        Target.create!(user: user, game: game)
         target2 = Target.create!(user: user2, game: game)
-
-        # target3 = Target.create!(user: user3, game: game)
-        # Target.create!(user: user4, game: game, active: false)
-
         Round.create!(name: 1, game: game)
         Round.assign_targets_to_actives(game)
-
-        # binding.pry
         expect(player1.target).to eq target2
       end
     end
